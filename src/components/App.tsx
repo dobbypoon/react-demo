bf2f5122f8e4bc86128636d66f0147d98e31a720
dobbypoon <dobby.sf.poon@gtomato.com>
2021-12-29 17:38:37 +0800

    add styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,57 @@ const Container = styled.div`
   margin-top: 40px;
 `
 
-const TodosWrapper = styled.div``
+const TodosWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+`
+
+const ActiveViewSelector = styled.div<{ $activeView: TActiveView }>`
+  [class^='active-view-btn-'] {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 3px 7px;
+
+    &:hover {
+      border-color: rgba(175, 47, 47, 0.1);
+    }
+  }
+
+  ${({ $activeView }) =>
+    `.active-view-btn-${$activeView} { border-color: rgba(175,47,47,0.15); }`}
+`
+
+const TodoItemWrapper = styled.div<{
+  $isEditing: boolean
+  $isCompleted: boolean
+}>`
+  [type='button'] {
+    opacity: 0;
+  }
+  &:hover {
+    [type='button'] {
+      opacity: 1;
+    }
+  }
+
+  [type='checkbox'],
+  [type='button'],
+  .item-description {
+    display: ${({ $isEditing }) => ($isEditing ? 'none' : 'initial')};
+  }
+
+  [type='text'] {
+    display: ${({ $isEditing }) => ($isEditing ? 'initial' : 'none')};
+  }
+
+  .item-description {
+    text-decoration: ${({ $isCompleted }) =>
+      $isCompleted ? 'line-through' : 'none'};
+    color: ${({ $isCompleted }) => ($isCompleted ? '#d9d9d9' : 'unset')};
+  }
+`
 
 enum ActiveViewType {
   ALL = 'ALL',
@@ -57,25 +107,38 @@ function TodoItem({ item, updateTodoItem }: TTodoItemProps): JSX.Element {
   }
 
   return (
-    <div>
+    /* TODO: if focused on other item, isEditing => false */
+    <TodoItemWrapper
+      $isEditing={isEditing}
+      $isCompleted={item.completed}
+      className="relative flex py-4 pl-10 pr-4"
+    >
       <input
+        className="absolute left-3 top-7"
         type="checkbox"
         onChange={handleToggleCompleted}
         checked={item.completed}
       />
-      <span onDoubleClick={handleEdit}>{item.description}</span>
-      {isEditing && (
+      <div className="relative flex grow">
+        <span
+          className="item-description grow text-2xl border border-transparent"
+          onDoubleClick={handleEdit}
+        >
+          {item.description}
+        </span>
         <input
+          className="w-full text-2xl outline-none border"
           type="text"
           ref={todoEditInputEl}
           defaultValue={item.description}
           onKeyDown={handleKeyDown}
         />
-      )}
+      </div>
+
       <button type="button" onClick={handleDeleteItem}>
         X
       </button>
-    </div>
+    </TodoItemWrapper>
   )
 }
 
@@ -154,14 +217,21 @@ function App(): JSX.Element {
   return (
     <Container>
       <TodosWrapper>
-        <div>
-          <h1>todos</h1>
-          <div>
-            {/* if all completed -> active */}
+        <div className="w-full flex flex-col items-center">
+          <h1 className="text-[100px] text-[rgba(175,47,47,0.15)] font-thin">
+            todos
+          </h1>
+          <div className="relative w-1/2 shadow-lg py-4 pl-10">
+            {/* TODO: if all completed -> active */}
             {todoList.length > 0 && (
-              <input type="checkbox" onChange={handleToggleAll} />
+              <input
+                className="absolute left-3 top-7"
+                type="checkbox"
+                onChange={handleToggleAll}
+              />
             )}
             <input
+              className="w-full text-2xl placeholder:italic placeholder:font-thin"
               type="text"
               ref={todoInputEl}
               placeholder="What needs to be done?"
@@ -169,48 +239,60 @@ function App(): JSX.Element {
             />
           </div>
         </div>
-        <div>
-          <ul>
-            {todoListDisplay.map((todoItem) => (
-              <li key={`todoItem_${todoItem.id}`}>
-                <TodoItem item={todoItem} updateTodoItem={updateTodoItem} />
-              </li>
-            ))}
-          </ul>
-          <div>
-            <span>
-              {`${
-                todoList.filter(({ completed }) => completed === false).length
-              } items left`}
-            </span>
-            <div>
-              <button
-                type="button"
-                onClick={() => setActiveView(ActiveViewType.ALL)}
+        {todoList.length > 0 && (
+          <div className="w-1/2 shadow-lg">
+            <ul className="divide-y divide-solid">
+              {todoListDisplay.map((todoItem) => (
+                <li key={`todoItem_${todoItem.id}`}>
+                  <TodoItem item={todoItem} updateTodoItem={updateTodoItem} />
+                </li>
+              ))}
+            </ul>
+            <div className="h-10 flex items-center py-3 px-4 border-t-[1px]">
+              <div className="w-1/3">
+                {`${
+                  todoList.filter(({ completed }) => completed === false).length
+                } items left`}
+              </div>
+              <ActiveViewSelector
+                $activeView={activeView}
+                className="w-1/3 flex justify-between"
               >
-                All
-              </button>
-              <button
-                type="button"
-                onClick={() => setActiveView(ActiveViewType.ACTIVE)}
-              >
-                Active
-              </button>
-              <button
-                type="button"
-                onClick={() => setActiveView(ActiveViewType.COMPLETED)}
-              >
-                Completed
-              </button>
+                <button
+                  type="button"
+                  className="active-view-btn-ALL"
+                  onClick={() => setActiveView(ActiveViewType.ALL)}
+                >
+                  All
+                </button>
+                <button
+                  type="button"
+                  className="active-view-btn-ACTIVE"
+                  onClick={() => setActiveView(ActiveViewType.ACTIVE)}
+                >
+                  Active
+                </button>
+                <button
+                  type="button"
+                  className="active-view-btn-COMPLETED"
+                  onClick={() => setActiveView(ActiveViewType.COMPLETED)}
+                >
+                  Completed
+                </button>
+              </ActiveViewSelector>
+              {todoList.filter(({ completed }) => completed === true).length >
+                0 && (
+                <button
+                  className="w-max ml-auto hover:underline"
+                  type="button"
+                  onClick={handleClearCompleted}
+                >
+                  Clear completed
+                </button>
+              )}
             </div>
-            {todoList.filter(({ completed }) => completed === true).length >
-              0 && (
-              <button type="button" onClick={handleClearCompleted}>
-                Clear completed
-              </button>
-            )}
           </div>
-        </div>
+        )}
       </TodosWrapper>
     </Container>
   )
